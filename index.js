e141e3dfa88d79fe590e8548b9d6520d8ee01831
muqtada <itsqdw@gmail.com>
2024-09-05 05:38:49 +0300

    to import

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const express = require('express')
+import express from 'express';
 const app = express()
-const port = 3000
+const port = 8080
 
 app.get('/', (req, res) => res.send('Hello World!'))
 app.listen(port, () => console.log(`listening on port ${port}!`))

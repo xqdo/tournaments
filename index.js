191935732ce52b7cca86a9e8a1bf31e1b9a5c61e
yousefahmed0 <hussansami20@gmail.com>
2024-09-05 05:43:45 +0300

    removed world

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ import express from 'express';
 const app = express()
 const port = 8080
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello !'))
 app.listen(port, () => console.log(`listening on port ${port}!`))

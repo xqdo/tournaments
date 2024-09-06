af910192fd693311460f6d2ddcf1c44740cfb6ae
muqtada <itsqdw@gmail.com>
2024-09-06 07:02:14 +0300

    logger & some endpoints

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 import express from 'express';
 import user from './routes/users.js'
 import tour from './routes/tour.js'
+
 const app = express()
 const port = 8080
 
+const logging = (req, res, next) =>
+{
+    console.log(`${req.method} - ${req.url}`);
+    next();
+}
+
+app.use(logging)
 app.use('/user', user)
 app.use('/tour', tour)
 
-app.get('/', (req, res) => res.send('Hello !'))
+app.get('/', (req, res) => res.send('bye !'))
 app.listen(port, () => console.log(`listening on port ${port}!`))
--- routes/users.js
+++ routes/users.js
@@ -2,21 +2,45 @@ import express from 'express';
 import { Sequelize, DataTypes } from 'sequelize';
 
 const router = express.Router();
+
 const db = new Sequelize('main', 'root', '1122', {
     host: 'localhost',
     dialect: 'mysql'
 });
+
 router.use(express.json())
 
 // Define the USER MODEL
 
 // THE CRUD API
 // DON'T USE JUST '/' ROUTES 
+//GET
+router.get('/all', (req, res) =>
+{
+    res.status(200).send("hello yousef");
+})
+router.get('/', (req, res) =>
+{
+    res.status(200).send("not yet implemented");
+})
+router.get('/:id', (req, res) =>
+{
+    const { id } = req.params
+    res.status(200).send("not yet implemented " + id);
+})
 //POST
 
-//GET
+router.post("/login", (req, res) =>
+{
+    const { username, password } = req.body
+    res.status(200).send("not yet implemented");
+})
 
-//PUT
+router.post("/login", (req, res) =>
+{
+    const { username, password } = req.body
+    res.status(200).send("not yet implemented");
+})
 
 //PATCH
 
